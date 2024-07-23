0dd095435c11774960285dad1f7dc32b2ccd7d0c
marcosscheffer <marcosscheffer2989@gmail.com>
2024-07-23 08:43:22 -0300

    Created dotenv

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+def add_num(num):
+    return num + 1
+
+class SimplesTestCase(TestCase):
+    def setUp(self):
+        self.numero = 41
+        print("iniciando testcase")
+
+    def test_add_num(self):
+        valor = add_num(self.numero)
+        self.assertTrue(valor == 42)
